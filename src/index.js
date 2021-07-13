452b7caac41c1397628ce32536ce0ee8ef1a2fda
Jhon9191 <joaoantonio15161516@gmail.com>
2021-07-13 19:58:48 -0300

    Creating first resolvers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-console.log('hello')
+import { ApolloServer, gql } from 'apollo-server';
 
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query{
+      hello: String
+      hi: String
+    }
+  `,
+
+    resolvers: {
+      Query: {
+        hello: () => {
+          return "Hello again";
+        },
+        hi: () => {
+          return "Hi again";
+        }
+      },
+    },
+
+});
+
+server.listen(4003).then(({url}) => {
+  console.log(`Server is running ${url}!`);
+}).catch(() => {
+  console.log("running o carai!");
+})
 
 
