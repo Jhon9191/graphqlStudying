8a50198ee3f3162cf0fce1161e0630539f34ce5a
Jhon9191 <joaoantonio15161516@gmail.com>
2021-10-23 08:53:19 -0300

    Scalar types

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -2,13 +2,41 @@ const { gql } = require("apollo-server");
 
 const rootTypeDefs = gql`
      type Query {
-      hi: String,
+        hi: String!
+        hello: String!
+        id: ID!
+        name: String!
+        age: Int!
+        average: Float!
+        married: Boolean!
+        arrayString: [String]!
     }
 
 `
 const rootResolvers = {
     Query: {
-        hi: () => "Ola"
+        id: async () => {
+            return 1
+        },
+        name: async () => {
+            return "João Antônio"
+        },
+        age: async () => {
+            return 30
+        },
+        average: async () => {
+            return 30.5
+        },
+        married: async () => {
+            return true
+        },
+        arrayString: async () => {
+            return ["Ola","Bom dia","João"]
+        }
+        // hi: () => "Ola",
+        // hello: async () => {
+        //     return "Olá 2"
+        // }   
     },
 };
 
