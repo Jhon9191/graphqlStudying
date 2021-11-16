930af04458af2e449f54dc04a2b8f6948e74c816
Jhon9191 <joaoantonio15161516@gmail.com>
2021-11-15 21:24:11 -0300

    Passing the id of user resolvers'

--- src/graphql/user/resolvers.js
+++ src/graphql/user/resolvers.js
@@ -2,15 +2,14 @@ import fetch from 'node-fetch';
 export const userResolvers = {
     
     Query: {
-        user: async () => {
-            return {
-                id: "sdsdasdasda2",
-                userName: "João Antônio"
-            }
+        user: async (_,{ id },{ hello }) => {
+            const response = await fetch("http://localhost:3000/users/" + id);
+            const user = await response.json();
+            //console.log(res);
+            return user;
         },
 
         users: async (_,__,{ hello }) => {
-            console.log(hello);
             const users = await fetch("http://localhost:3000/users")
             return users.json();
         },
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { ApolloServer, gql } from 'apollo-server';
 import { typeDefs, resolvers } from './graphql/schema';
+import fetch from 'node-fetch';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: () => {
     return {
-      hello: 123
+      fetch
     }
   }
 });
