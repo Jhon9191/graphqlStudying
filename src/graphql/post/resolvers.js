f4cf0cb9065d8f48b99515cfb818b7ac71f8d8c4
Jhon9191 <joaoantonio15161516@gmail.com>
2021-11-29 21:53:49 -0300

    Implementing Apollo RESTDataSource

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -1,15 +1,12 @@
-const post = async (_, { id }, { getPosts }) => {
-    const response = await getPosts("/" + id);
-    const post = await response.json();
+const post = async (_, { id }, { dataSources }) => {
+    const post = dataSources.postApi.getPost(id);
     return post;
 };
 
-const posts = async (_, { input }, { getPosts }) => {
-    const ApiFiltersInput = new URLSearchParams(input)
-    const response = await getPosts("/?" + ApiFiltersInput);
-    const posts = await response.json();
+const posts = async (_, { input }, { dataSources }) => {
+    const posts = dataSources.postApi.getPosts(input);
     return posts;
-}
+};
 
 const user = async ({ userId }, _, { userDataLoader }) => {
     return userDataLoader.load(userId);
